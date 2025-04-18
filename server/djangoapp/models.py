4cf75da2869b4af2d3bbcf30452e2d583090d5d5
suikedigital <130358661+suikedigital@users.noreply.github.com>
2025-04-18 15:45:38 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -23,12 +23,13 @@ class CarModel(models.Model):
 
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
 
-    year = models.IntegerField(default=2025,
-                               validators=[
-                                   MaxValueValidator(2025),
-                                   MinValueValidator(2015)
-                               ]
-                            )
+    year = models.IntegerField(
+        default=2025,
+        validators=[
+            MaxValueValidator(2025),
+            MinValueValidator(2015)
+        ]
+    )
 
     def __str__(self):
         return self.name
