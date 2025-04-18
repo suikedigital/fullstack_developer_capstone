f7cafee30ec28c9ea1a1fdd91210778db8a72740
suikedigital <130358661+suikedigital@users.noreply.github.com>
2025-04-18 17:15:07 +0100

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -26,50 +26,32 @@ urlpatterns = [
     ),
     path(
         'djangoapp/',
-        include('djangoapp.urls'
-               )
+        include('djangoapp.urls')
     ),
     path(
         '',
-        TemplateView.as_view(
-            template_name="Home.html"
-        )
+        TemplateView.as_view(template_name="Home.html")
     ),
     path(
         'about/',
-        TemplateView.as_view(
-            template_name="About.html"
-        )
+        TemplateView.as_view(template_name="About.html")
     ),
     path('contact/',
-         TemplateView.as_view(
-             template_name="Contact.html"
-         )
+         TemplateView.as_view(template_name="Contact.html")
         ),
     path(
         'login/',
-        TemplateView.as_view(
-            template_name="index.html"
-        )
+        TemplateView.as_view(template_name="index.html")
     ),
     path(
-        'dealers/',
-        TemplateView.as_view(
-            template_name="index.html"
-        )
-    )
-    ,
+        'dealers/',TemplateView.as_view(template_name="index.html")
+    ),
     path(
-        'dealer/<int:dealer_id>',
-        TemplateView.as_view(
-            template_name="index.html"
-        )
+        'dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")
     ),
     path(
         'postreview/<int:dealer_id>',
-        TemplateView.as_view(
-            template_name="index.html"
-        )
+        TemplateView.as_view(template_name="index.html")
     ),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
