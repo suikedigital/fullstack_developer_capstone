8ba1ce41e4c9199285c1222334b21705d9d79bd9
suikedigital <130358661+suikedigital@users.noreply.github.com>
2025-04-18 16:37:42 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,44 +9,44 @@ urlpatterns = [
 
     # path for login
     path(
-        route = 'login',
-        view = views.login_user,
-        name = 'login'
+        route='login',
+        view=views.login_user,
+        name='login'
     ),
     path(
-        route = 'logout',
-        view = views.logout_request,
-        name = 'logout'
+        route='logout',
+        view=views.logout_request,
+        name='logout'
     ),
     path(
-        route = 'get_cars',
-        view = views.get_cars,
-        name ='getcars'
+        route='get_cars',
+        view=views.get_cars,
+        name='getcars'
     ),
     path(
-        route = 'get_dealers/',
-        view = views.get_dealerships,
-        name = 'get_dealers'
+        route='get_dealers/',
+        view=views.get_dealerships,
+        name='get_dealers'
     ),
     path(
-        route = 'get_dealers/<str:state>',
-        view = views.get_dealerships,
-        name = 'get_dealers_by_state'
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
     ),
     path(
-        route = 'dealer/<int:dealer_id>',
-        view = views.get_dealer_details,
-        name = 'dealer_details'
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
     ),
     path(
-        route = 'reviews/dealer/<int:dealer_id>',
-        view = views.get_dealer_reviews,
-        name = 'dealer_details'
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_details'
     ),
     path(
-        route = 'reviews/dealer/add',
-        view = views.add_review,
-        name = 'add_review'
+        route='reviews/dealer/add',
+        view=views.add_review,
+        name='add_review'
     ),
 
 
