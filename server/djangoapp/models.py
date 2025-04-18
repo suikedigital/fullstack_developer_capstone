38c01d50acf2dfab1e2166cdd25653de1dd3fe3d
suikedigital <130358661+suikedigital@users.noreply.github.com>
2025-04-18 15:31:53 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,7 +8,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
-        
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
